f569743a2496866bb1a4516ba97a66f83701c525
robert.lee <robert0714@gmail.com>
2018-12-30 16:01:52 +0800

    adjust for spring-boot 2.1.1

--- Chapter04/spring-data-redis-demo/src/main/java/packt/rediscookbook/springdataredisdemo/model/User.java
+++ Chapter04/spring-data-redis-demo/src/main/java/packt/rediscookbook/springdataredisdemo/model/User.java
@@ -1,16 +1,29 @@
 package packt.rediscookbook.springdataredisdemo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Value;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 @Data
 @AllArgsConstructor
+@Value
+@Builder
+@JsonDeserialize(builder = User.UserBuilder.class)
 public class User implements Serializable {
     private String id;
     private String name;
     private String sex;
     private String nation;
     private long register_time;
+    
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class UserBuilder {
+    }
+
 }
